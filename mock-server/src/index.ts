26a131cbf2e9ab6e2d0d29589f18484e273e0e7c
Deep Majumder <deep.majumder2019@gmail.com>
2021-03-07 23:55:39 +0530

    Some user api endpoints made

--- mock-server/src/index.ts
+++ mock-server/src/index.ts
@@ -1,11 +1,84 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
-app.get('/', (req, res) => {
-    console.log(req);
-    res.send('Hello, World!');
+app.use(cors());
+
+interface User {
+    id: string,
+    username: string,
+    password: string,
+    type: string,
+}
+
+let users: User[] =
+    [
+        {
+            "id": "abc",
+            "username": "root",
+            "password": "toor",
+            "type": "Manager"
+        },
+        {
+            "id": "rev",
+            "username": "deep",
+            "password": "noneshallpass",
+            "type": "Customer"
+        },
+        {
+            "id": "whatever",
+            "username": "ramu",
+            "password": "paisabazar",
+            "type": "Salesperson"
+        },
+        {
+            "id": "hell",
+            "username": "borda",
+            "password": "cha-kothae",
+            "type": "Accountant",
+        }
+    ];
+
+app.get('/users', (_req, res) => {
+    res.send(users);
+});
+
+app.get('/users/info/:id', (req, res) => {
+    const id = req.params.id;
+    for (const user of users) {
+        if (user.id === id) {
+            res.send(user);
+            return;
+        }
+    }
+    res.send("");
+});
+
+app.post('/users', (req, res) => {
+    res.send(req.body);
+});
+
+app.delete('/users', (req, res) => {
+    res.send(req.body);
+});
+
+app.patch('/users', (req, res) => {
+    res.send(req.body);
+});
+
+app.get('/users/login', (req, res) => {
+    const username = req.query.username;
+    const password = req.query.password;
+    console.log(`username = ${username}  password = ${password}`);
+    for (const user of users) {
+        if (user.username === username && user.password === password) {
+            res.json(user);
+            return;
+        }
+    }
+    res.send("");
 });
 
 app.listen(port, () => {
