e9490064e74487e57850b55da884f5c250f369b8
Deep Majumder <deep.majumder2019@gmail.com>
2021-03-11 23:17:12 +0530

    Initial backend

---
+++ backend/src/test/java/eternal/blue/sams/SamsApplicationTests.java
@@ -0,0 +1,13 @@
+package eternal.blue.sams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
