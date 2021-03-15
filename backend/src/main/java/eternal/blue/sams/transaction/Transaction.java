ea4cf2b9ea7239128eb3528f06799ab21a85436c
Deep Majumder <deep.majumder2019@gmail.com>
2021-03-15 19:52:34 +0530

    Transaction model created

---
+++ backend/src/main/java/eternal/blue/sams/transaction/Transaction.java
@@ -0,0 +1,102 @@
+package eternal.blue.sams.transaction;
+
+import eternal.blue.sams.user.UserType;
+import org.springframework.data.annotation.Id;
+
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * Transaction represents a single monetary transaction in SAMS.
+ * All forms of payments/refunds are expressed in terms of Transactions.
+ */
+public class Transaction {
+    @Id
+    private BigInteger id;
+    private double amount;
+    private BigInteger initiatorId;
+    private BigInteger showId;
+    private UserType initiatorType;
+    private LocalDateTime time;
+
+    public Transaction(double amount, BigInteger initiatorId, BigInteger showId, UserType initiatorType, LocalDateTime time) {
+        this.amount = amount;
+        this.initiatorId = initiatorId;
+        this.showId = showId;
+        this.initiatorType = initiatorType;
+        this.time = time;
+    }
+
+    public Transaction() {
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", initiatorId=" + initiatorId +
+                ", showId=" + showId +
+                ", initiatorType=" + initiatorType +
+                ", time=" + time +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transaction that = (Transaction) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public BigInteger getInitiatorId() {
+        return initiatorId;
+    }
+
+    public void setInitiatorId(BigInteger initiatorId) {
+        this.initiatorId = initiatorId;
+    }
+
+    public BigInteger getShowId() {
+        return showId;
+    }
+
+    public void setShowId(BigInteger showId) {
+        this.showId = showId;
+    }
+
+    public UserType getInitiatorType() {
+        return initiatorType;
+    }
+
+    public void setInitiatorType(UserType initiatorType) {
+        this.initiatorType = initiatorType;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+}
