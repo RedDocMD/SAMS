b29523402ca3f74634046687fcd4703889ac62d9
Deep Majumder <deep.majumder2019@gmail.com>
2021-03-19 15:17:34 +0530

    Created Expenditure controller

---
+++ backend/src/main/java/eternal/blue/sams/expenditure/ExpenditureController.java
@@ -0,0 +1,62 @@
+package eternal.blue.sams.expenditure;
+
+import eternal.blue.sams.SamsApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+import java.util.List;
+
+@CrossOrigin(SamsApplication.apiConsumerAddress)
+@RestController
+@RequestMapping("/expenditures")
+public class ExpenditureController {
+    private final ExpenditureService expenditureService;
+
+    public ExpenditureController(ExpenditureService expenditureService) {
+        this.expenditureService = expenditureService;
+    }
+
+    @GetMapping("")
+    public List<Expenditure> all() {
+        return expenditureService.getAllExpenditures();
+    }
+
+    @PostMapping("")
+    public Expenditure create(@RequestBody ExpenditureCreation expenditureCreation) {
+        return expenditureService.createExpenditure(expenditureCreation.getExpenditure(), expenditureCreation.getAccountantId());
+    }
+
+    @GetMapping("/{id:\\d+}")
+    public Expenditure getOne(@PathVariable BigInteger id) {
+        return expenditureService.getExpenditure(id).orElse(null);
+    }
+
+    @GetMapping("/by_show/{id:\\d+}")
+    public List<Expenditure> byShow(@PathVariable("id") BigInteger showId) {
+        return expenditureService.getExpendituresByShow(showId);
+    }
+
+    public static class ExpenditureCreation {
+        private Expenditure expenditure;
+        private BigInteger accountantId;
+
+        public ExpenditureCreation() {
+        }
+
+        public Expenditure getExpenditure() {
+            return expenditure;
+        }
+
+        public void setExpenditure(Expenditure expenditure) {
+            this.expenditure = expenditure;
+        }
+
+        public BigInteger getAccountantId() {
+            return accountantId;
+        }
+
+        public void setAccountantId(BigInteger accountantId) {
+            this.accountantId = accountantId;
+        }
+    }
+}
