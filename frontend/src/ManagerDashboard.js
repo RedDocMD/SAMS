41066b6ecac7c86276d6e7b09de4be54ed4152a2
Deep Majumder <deep.majumder2019@gmail.com>
2021-04-03 18:33:12 +0530

    Basic framework of Manager Dashboard

--- frontend/src/ManagerDashboard.js
+++ frontend/src/ManagerDashboard.js
@@ -1,9 +1,13 @@
 import ViewShows from './ViewShows'
-import { Box, Button, Container, Grid, Typography } from '@material-ui/core'
+import { makeStyles, Box, Container, Grid, Typography, List, ListItem, ListItemText, ListItemIcon, Collapse } from '@material-ui/core'
 import React, { useState } from 'react'
+import MovieIcon from '@material-ui/icons/Movie'
+import AddIcon from '@material-ui/icons/Add'
+import ListIcon from '@material-ui/icons/List'
 
 import CreateAccount from './CreateAccount'
 import ViewAccountants from './ViewAccountants'
+import { ExpandLess, ExpandMore } from '@material-ui/icons'
 
 let ManagerDashboardEnum = Object.freeze({
     'default': 1,
@@ -12,12 +16,25 @@ let ManagerDashboardEnum = Object.freeze({
     'viewShows': 4,
 })
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+    nested: {
+        paddingLeft: theme.spacing(4),
+    },
+}))
+
 function ManagerDashboard(props) {
-    let [viewState, setViewState] = useState(1)
-    let createAccountHandler = () => {
+    const classes = useStyles()
+    const [viewState, setViewState] = useState(1)
+
+    const createAccountHandler = () => {
         setViewState(ManagerDashboardEnum.createAccount)
     }
-    let dashboardCallback = () => {
+    const dashboardCallback = () => {
         setViewState(ManagerDashboardEnum.default)
     }
     let viewShowsHandler = () => {
@@ -27,31 +44,55 @@ function ManagerDashboard(props) {
         setViewState(ManagerDashboardEnum.showAccountants)
     }
 
+    const [showsOpen, setShowsOpen] = useState(false)
+    const [accountsOpen, setAccountsOpen] = useState(false)
 
-    let dashboard = (<Container>
-        <Grid container spacing={3}>
-            <Grid item xs={12}>
-                <Box>
-                    <Typography variant="h3" align="center">
-                        Manager Dashboard
-                    </Typography>
-                </Box>
+    let dashboard = (
+        <Container>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Box mt={3}>
+                        <Typography variant="h3" align="center">
+                            Manager Dashboard
+                        </Typography>
+                    </Box>
+                </Grid>
+                <Grid item xs={4}>
+                    <List classNames={classes.root}>
+                        <ListItem button onClick={() => setShowsOpen(!showsOpen)}>
+                            <ListItemIcon>
+                                <MovieIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Shows" />
+                            {showsOpen ? <ExpandLess /> : <ExpandMore />}
+                        </ListItem>
+                        <Collapse in={showsOpen} timeout="auto" unmountOnExit>
+                            <List disablePadding>
+                                <ListItem button className={classes.nested}>
+                                    <ListItemIcon>
+                                        <AddIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary="Create Show" />
+                                </ListItem>
+                                <ListItem button className={classes.nested}>
+                                    <ListItemIcon>
+                                        <ListIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary="View Shows" />
+                                </ListItem>
+                            </List>
+                        </Collapse>
+                        <ListItem button onClick={() => setAccountsOpen(!accountsOpen)}>
+                            <ListItemIcon></ListItemIcon>
+                            <ListItemText primary="Accounts" />
+                            {accountsOpen ? <ExpandLess /> : <ExpandMore />}
+                        </ListItem>
+                    </List>
+                </Grid>
             </Grid>
-            <Grid item xs={3}>
-                <Button variant="contained" color="primary" onClick={createAccountHandler}>
-                    Create Account
-                </Button>
-            </Grid>
-            <Grid item xs={3}>
-                <Button variant="contained" color="primary" onClick={showAccountantHandle}>
-                    Show Accountant
-                </Button>
-                <Button variant="contained" color="primary" onClick={viewShowsHandler}>
-                    View Shows
-                </Button>
-            </Grid>
-        </Grid>
-    </Container>)
+        </Container>
+    )
+
     let createAccountView = <CreateAccount callback={dashboardCallback} baseURL = {props.baseURL} />
     let showAccountantView = <ViewAccountants callback={dashboardCallback} baseURL = {props.baseURL} />
     let viewShowsView = <ViewShows callback = {dashboardCallback} baseURL = {props.baseURL}/>
