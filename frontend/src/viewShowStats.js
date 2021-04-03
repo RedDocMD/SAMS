4a5a6e5564f5b0926f8411be4999ae0acd2d6b2c
Aaditya Agrawal <aaditya1106@gmail.com>
2021-04-03 21:27:10 +0530

    View Shows and Show Stats

---
+++ frontend/src/viewShowStats.js
@@ -0,0 +1,147 @@
+import {Box, Button, Typography, Container, Grid, TextField} from '@material-ui/core'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import PropTypes from 'prop-types'
+import JSONbig from 'json-bigint'
+import { makeStyles } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
+
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+})
+
+
+function ViewShowStats(props) {
+    let createHandler = () => {
+        props.callback()
+    }
+    const classes = useStyles()
+
+    let [tickets, setTickets] = useState([])
+    let [transactions, setTransactions] = useState([])
+
+    const bigIntToString = num => {
+        const parts = num.c
+        let res = ''
+        for (const part of parts) {
+            res = res.concat(part.toString())
+        }
+        return res
+    }
+
+
+    useEffect(() => {
+        const fetchTickets = async () => {
+            try{
+                let url =  `${props.baseURL}/tickets/by_show/${bigIntToString(props.show.id)}`
+                setTickets([])
+                const response = await axios.get(url,{transformResponse: data => data})
+                const json = JSONbig.parse(response.data)
+                setTickets(json)
+                url = `${props.baseURL}/transactions/by_show/${bigIntToString(props.show.id)}`
+                const response1 = await axios.get(url,{transformResponse: data => data})
+                const json1 = JSONbig.parse(response1.data)
+                setTransactions(json1)
+            }catch (e){
+                setTickets([])
+            }
+        }
+        fetchTickets()
+    },[])
+    let findCount = (flag) => {
+        let cnt = 0
+        for (var i in tickets){
+            if (flag == 0 && tickets[i].type == 'Regular'){
+                cnt++
+            }
+            if (flag == 1 && tickets[i].type == 'Balcony'){
+                cnt++
+            }
+        }
+        return cnt
+    }
+    return (
+        <Container>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Box mt={3}>
+                        <Typography variant="h4" align="center">
+                            Show Stats
+                        </Typography>
+                    </Box>
+                </Grid>
+                <Grid item xs={2} />
+                <Grid item xs={4}>
+                    Number of Regular Seats Booked : {findCount(0)}
+                </Grid>
+                <Grid item xs={4}>
+                    Percentage of Regular Seats Booked : {props.show.regularTicketCount == 0?0:findCount(0)/props.show.regularTicketCount}%
+                </Grid>
+                <Grid item xs={2} />
+                <Grid item xs={2} />
+                <Grid item xs={4}>
+                    Number of Balcony Seats Booked : {findCount(1)}
+                </Grid>
+                <Grid item xs={4}>
+                    Percentage of Regular Seats Booked : {props.show.balconyTicketCount == 0?0:findCount(1)/props.show.balconyTicketCount}%
+                </Grid>
+                <Grid item xs={2} />
+                <Grid item xs={12}>
+                    <Box mt={3}>
+                        <Typography variant="h6" align="center">
+                            Balance Sheet
+                        </Typography>
+                    </Box>
+                </Grid>
+                <Grid item xs = {12}>
+                    <TableContainer component={Paper}>
+                        <Table className={classes.table} aria-label="simple table">
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Type</TableCell>
+                                    <TableCell align="right">Time</TableCell>
+                                    <TableCell align="right">Amount</TableCell>
+                                    <TableCell align="right">Initiator</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {transactions.map((transaction) => (
+                                    <TableRow key={transaction.id}>
+                                        <TableCell component="th" scope="row">
+                                            {transaction.type}
+                                        </TableCell>
+                                        <TableCell align="right">{transaction.time}</TableCell>
+                                        <TableCell align="right">{transaction.amount}</TableCell>
+                                        <TableCell align="right">{transaction.initiatorType}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+                <Grid item xs={5} />
+                <Grid item xs={2} mt={3}>
+                    <Button fullWidth variant="contained" color="primary" onClick={createHandler}>Go Back</Button>
+                </Grid>
+                <Grid item xs={5} />
+            </Grid>
+        </Container>
+
+    )
+}
+
+ViewShowStats.propTypes = {
+    baseURL: PropTypes.string.isRequired,
+    callback: PropTypes.func.isRequired,
+    show: PropTypes.object.isRequired,
+}
+
+export default ViewShowStats
