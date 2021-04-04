43e8abf912407bf0ce513c487ca4f25a8acaddd7
Deep Majumder <deep.majumder2019@gmail.com>
2021-04-04 23:31:32 +0530

    Basic table view

--- frontend/src/ViewSalesperson.js
+++ frontend/src/ViewSalesperson.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
-import { Box, Container, Grid, Typography } from '@material-ui/core'
+import { Box, Button, Container, Grid, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@material-ui/core'
 import axios from 'axios'
 const JSONbig = require('json-bigint')({ storeAsString: true })
+import collect from 'collect.js'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 function ViewSalesperson(props) {
     const [salespersons, setSalespersons] = useState([])
+    const [transactionMap, setTransactionMap] = useState({})
 
     useEffect(() => {
         (async () => {
@@ -15,14 +18,43 @@ function ViewSalesperson(props) {
                 const users = JSONbig.parse(response.data)
                 const salespersons = users.filter(user => user.type === 'Salesperson')
                 setSalespersons(salespersons)
+                let theMap = {}
+                for (const salesperson of salespersons) {
+                    const newUrl = `${props.baseURL}/transactions/by_salesperson/${salesperson.id}`
+                    const newResponse = await axios.get(newUrl, {transformResponse: data => data})
+                    const transactions = JSONbig.parse(newResponse.data)
+                    theMap[salesperson.id] = transactions
+                }
+                setTransactionMap(theMap)
             } catch (e) {
                 console.error(e)
                 setSalespersons([])
+                setTransactionMap({})
             }
         })()
     }, [])
-    
-    console.log(salespersons)
+
+    let salespersonItems = salespersons.map(salesperson => {
+        const transactions = collect(transactionMap[salesperson.id])
+        const totalMoney = transactions.reduce((carry, item) => carry + item.amount, 0)
+        const totalTickets = transactions.count()
+        const name = salesperson.username
+        return (
+            <TableRow key={salesperson.id}>
+                <TableCell>{name}</TableCell>
+                <TableCell>{totalTickets}</TableCell>
+                <TableCell>{`₹ ${totalMoney}`}</TableCell>
+                <TableCell>
+                    <Button variant='contained' color='primary'>All Transactions</Button>
+                </TableCell>
+                <TableCell>
+                    <IconButton color='secondary'>
+                        <DeleteIcon />
+                    </IconButton>
+                </TableCell>
+            </TableRow>
+        )
+    })
 
     return (
         <Container>
@@ -34,6 +66,26 @@ function ViewSalesperson(props) {
                         </Typography>
                     </Box>
                 </Grid>
+                <Grid item xs={2} />
+                <Grid item xs={8}>
+                    <TableContainer component={Paper}>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Username</TableCell> 
+                                    <TableCell>Tickets sold</TableCell> 
+                                    <TableCell>Total credit</TableCell>
+                                    <TableCell />
+                                    <TableCell />
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {salespersonItems}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+                <Grid item xs={2} />
             </Grid>
         </Container>
     )
