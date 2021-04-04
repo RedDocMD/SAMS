a56032c644184be9a59efe1070a7825e0f4ec393
Deep Majumder <deep.majumder2019@gmail.com>
2021-04-04 12:15:14 +0530

    Date and time pickers added

--- frontend/src/CreateShow.js
+++ frontend/src/CreateShow.js
@@ -1,31 +1,56 @@
-import { Container, Grid, Typography, Box, FormControl, TextField } from '@material-ui/core'
-import { Label } from '@material-ui/icons'
+import { Container, Grid, Typography, Box, TextField } from '@material-ui/core'
 import React, { useState } from 'react'
+import { MuiPickersUtilsProvider, KeyboardDatePicker, KeyboardTimePicker } from '@material-ui/pickers'
+import DateFnsUtils from '@date-io/date-fns'
+import 'date-fns'
 
 function CreateShow(props) {
     const [name, setName] = useState('')
+    const [dateTime, setDateTime] = useState(new Date())
+    
+    console.log(dateTime)
 
     return (
-        <Container>
-            <Grid container spacing={6}>
-                <Grid item xs={12}>
-                    <Box mt={3}>
-                        <Typography variant='h4' align='center'>
-                            Create Show
-                        </Typography>
-                    </Box>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Container>
+                <Grid container spacing={6}>
+                    <Grid item xs={12}>
+                        <Box mt={3}>
+                            <Typography variant='h4' align='center'>
+                                Create Show
+                            </Typography>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={3} />
+                    <Grid item xs={6}>
+                        <TextField fullWidth variant='outlined' label='Show Name' onChange={ev => setName(ev.target.value)}/>
+                    </Grid>
+                    <Grid item xs={3} />
+                    <Grid item xs={3} />
+                    <Grid item xs={6}>
+                        <Box display='flex' justifyContent='space-between'>
+                            <KeyboardDatePicker
+                                disableToolbar
+                                variant='inline'
+                                format='dd/MM/yyyy'
+                                label='Show Date'
+                                value={dateTime}
+                                onChange={date => setDateTime(date)}
+                            />
+                            <KeyboardTimePicker
+                                variant='inline'
+                                id='time-picker'
+                                label='Time picker'
+                                value={dateTime}
+                                onChange={time => setDateTime(time)}
+                            />
+
+                        </Box>
+                    </Grid>
+                    <Grid item xs={3} />
                 </Grid>
-                <Grid item xs />
-                <Grid item xs={6}>
-                    <TextField fullWidth variant='outlined' label='Show Name' onChange={ev => setName(ev.target.value)}/>
-                </Grid>
-                <Grid item xs />
-                <Grid item xs />
-                <Grid item xs={6}>
-                </Grid>
-                <Grid item xs />
-            </Grid>
-        </Container>
+            </Container>
+        </MuiPickersUtilsProvider>
     )
 }
 
